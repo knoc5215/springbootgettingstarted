f89a92f311595a9fb3e52dda962036f792519c61
knoc5215 <knoc5215@naver.com>
2020-12-23 00:15:37 +0900

    내장 웹 서버 응용 2부 : HTTPS와 HTTP2

---
+++ src/main/java/httpsAndHttp2/httpsAndHttp2Application.java
@@ -0,0 +1,38 @@
+package httpsAndHttp2;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class httpsAndHttp2Application {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring";
+    }
+
+    // HTTP multi connector
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);    // http connector
+        return connector;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(httpsAndHttp2Application.class, args);
+    }
+}
